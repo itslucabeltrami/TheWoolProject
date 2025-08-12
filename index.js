2f857e174410b41b54c452d525c28536c097d549
itslucabeltrami <lucasi.iscrive@gmail.com>
2025-08-12 16:45:14 +0200

    Cart logic + bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // products lists and information
 const products = [
+    //WOMAN SECTION
     {
         id: 1,
         name: 'Cashmere turtleneck sweater',
@@ -63,9 +64,7 @@ const products = [
         image: '242MD5329938-C001-F.webp',
         category: ['coats', 'woman'],
     },
-
     //MAN SECTION
-
     {
         id: 20,
         name: 'Chevron Blazer',
@@ -89,7 +88,7 @@ const products = [
     },
     {
         id: 23,
-        name: 'Test Name',
+        name: 'Shoes',
         price: '900,00',
         image: '242MZUNICK771-C8291-F.webp',
         category: ['shoes', 'man'],
@@ -104,14 +103,91 @@ const products = [
 
 
 ];
+
 let cartProductsToBuy = []
-
-
 
 let toTop = $('#to-top-btn');
 
-$(document).on('scroll', function(){ scrollFunction() })
+$(document).ready(() => {
 
+    initApp();
+
+    updateCartCounter();
+
+    productsToPrint();
+
+    $(document).on('scroll', function(){ scrollFunction() })
+
+    // cart logic
+    const cartIcon = $("#cart-button-action");
+    const cart = $('.cart');
+    const cartCLose = $('.cart-close-button');
+    const cartCloseArrow = $('.cart-close-arrow-button');
+    const cartContentSideMenu = $('.cart-content-side-menu');
+
+    cartIcon.on('click', function () {
+        cart.addClass('active')
+        cartContentSideMenu.fadeIn()
+
+    });
+    cartCLose.on('click', function () {
+        cart.removeClass('active')
+        cartContentSideMenu.hide()
+    });
+
+    cartContentSideMenu.on('click', function () {
+        cart.removeClass('active')
+        cartContentSideMenu.hide()
+    });
+
+    cartCloseArrow.on('click', function () {
+        cart.removeClass('active')
+        cartContentSideMenu.fadeOut()
+    });
+})
+
+// INIT APP
+function initApp() {
+    cartProductsToBuy = JSON.parse(localStorage.getItem('cart-products'));
+
+    const cartToBuyElements = ''
+    let cartToBuyTotalPrice = 0
+    const cartContent = $('#cart-content-box');
+    const cartContentPrice = $('#cart-content-price-counter');
+
+    cartContent.text("");
+    cartProductsToBuy.forEach((element) => {
+        const priceCounted = parseFloat(element.price.replace(',', '.')) * element.count;
+        cartToBuyTotalPrice += priceCounted;
+
+        let toPrintVar = cartToBuyElements;
+
+        toPrintVar += `
+            <div class="cart-box" data-id="${element.id}">
+                <img src="./assets/img/${element.image}" class="cart-img">
+                <div class="cart-detail">
+                      <h2 class="cart-product-title">${element.name}</h2>
+                      <i class="cart-price">${element.price} €</i>
+                      <div class="cart-quantity">
+                          <button id="decrement" onclick="cartItemActionQuantity('minus', ${element.id})">-</button>
+                          <span id="cartToBuyActionCount" class="number">${element.count}</span>
+                          <button id="increment" onclick="cartItemActionQuantity('plus', ${element.id})">+</button>
+                      </div>
+                  </div>
+                  <div onclick="removeToCartElement(${element.id})">
+                <img src="./assets/img/delete-bin-7-line.png" class="cart-remove"/>
+                </div>
+            </div>
+        `;
+        cartContent.append(toPrintVar);
+    })
+
+    cartContentPrice.text("")
+    cartContentPrice.append(cartToBuyTotalPrice.toFixed(2).toString().replace('.', ',')+'€')
+
+}
+
+// SCROLL
 function scrollFunction() {
     if (document.documentElement.scrollTop > 20) {
         toTop.fadeIn(500);
@@ -120,42 +196,47 @@ function scrollFunction() {
     }
 }
 
+// SCROLL TO TOP
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome
 }
 
+// PRODUCTS TO PRINT
+function productsToPrint() {
+    let toPrint = '';
 
-let toPrint = '';
-const productsContainer = $('#products-container-to-show');
-const page = $(location).prop('href').split('/');
-const pageCategory = page[page.length - 1].split('.')[0].toString()
+    const productsContainer = $('#products-container-to-show');
+    const page = $(location).prop('href').split('/');
+    const pageCategory = page[page.length - 1].split('.')[0].toString()
 
-for (const product of products) {
-    // copy of original
-    let toPrintVar = '';
+    for (const product of products) {
+        // copy of original
+        let toPrintVar = '';
 
-    if (product.category.find((i) => i === pageCategory)) {
-        toPrintVar += `<div class="card-product" data-id="${product.id}">`
-        toPrintVar += '<div class="card-product-container">'
-        toPrintVar += '<div class="card-product-image">'
-        toPrintVar += `<img src="./assets/img/${product.image}">`
-        toPrintVar += '</div>'
-        toPrintVar += '<div class="product-name-style">' + product.name + '</div>'
-        toPrintVar += '<div  class="product-price-style">' + product.price + '€</div>'
-        toPrintVar += `<div class="add-to-cart-button" onclick="addToCartExample(${product.id})">add to cart</div>`
-        toPrintVar += '</div>'
-        toPrintVar += '</div>'
+        if (product.category.find((i) => i === pageCategory)) {
+            toPrintVar += `<div class="card-product" data-id="${product.id}">`
+            toPrintVar += '<div class="card-product-container">'
+            toPrintVar += '<div class="card-product-image">'
+            toPrintVar += `<img src="./assets/img/${product.image}">`
+            toPrintVar += '</div>'
+            toPrintVar += '<div class="product-name-style">' + product.name + '</div>'
+            toPrintVar += '<div  class="product-price-style">' + product.price + '€</div>'
+            toPrintVar += `<div class="add-to-cart-button" onclick="addToCartProcessor(${product.id})">add to cart</div>`
+            toPrintVar += '</div>'
+            toPrintVar += '</div>'
+        }
+
+        // back to original copy
+        toPrint += toPrintVar;
     }
-
-    // back to original copy
-    toPrint += toPrintVar;
+    productsContainer.append(toPrint)
 }
-productsContainer.append(toPrint)
 
 // filtering products with category button
-
 function filterProducts(type, gender) {
+    const productsContainer = $('#products-container-to-show');
+
     //removing previous products
     productsContainer.children().remove()
     productsContainer.hide()
@@ -173,7 +254,7 @@ function filterProducts(type, gender) {
             toPrintVar += '</div>'
             toPrintVar += '<div class="product-name-style">' + product.name + '</div>'
             toPrintVar += '<div  class="product-price-style">' + product.price + '€</div>'
-            toPrintVar += `<div class="add-to-cart-button" onclick="addToCartExample(${product.id})">add to cart</div>`
+            toPrintVar += `<div class="add-to-cart-button" onclick="addToCartProcessor(${product.id})">add to cart</div>`
             toPrintVar += '</div>'
             toPrintVar += '</div>'
         }
@@ -216,31 +297,6 @@ function showAll(type) {
     productsContainer.fadeIn(500);
 }
 
-// cart logic
-const cartIcon = $("#cart-button-action");
-const cart = $('.cart');
-const cartCLose = $('.cart-close-button');
-const cartCloseArrow = $('.cart-close-arrow-button');
-
-//opening and closing
-cartIcon.on('click', function () {
-    cart.addClass('active')
-});
-cartCLose.on('click', function () {
-    cart.removeClass('active')
-});
-cartCloseArrow.on('click', function () {
-    cart.removeClass('active')
-});
-
-// adding products
-const addCartButtons = document.querySelectorAll('.add-to-cart-button');
-addCartButtons.addEventListener('click', event => {
-    const productBox = event.target.closest('.card-product');
-    addToCart(productBox);
-});
-
-
 function addToCart(product) {
     const cartContent = $('#cart-content-box');
     const cartContentPrice = $('#cart-content-price-counter');
@@ -248,18 +304,16 @@ function addToCart(product) {
     if (product) {
         const isInCart = cartProductsToBuy.find((i) => i.id === product.id);
 
-        if (isInCart) {
+        if (isInCart !== undefined) {
             isInCart.count += 1;
-            cartContent.text("")
-
         } else {
             cartProductsToBuy.push({
                 ...product,
                 count: 1,
             });
         }
-    }
 
+    }
 
     const cartToBuyElements = ''
     let cartToBuyTotalPrice = 0
@@ -286,7 +340,7 @@ function addToCart(product) {
                   </div>
                   <div onclick="removeToCartElement(${element.id})">
                 <img src="./assets/img/delete-bin-7-line.png" class="cart-remove"/>
-</div>
+                </div>
             </div>
         `;
 
@@ -297,7 +351,11 @@ function addToCart(product) {
     cartContentPrice.append(cartToBuyTotalPrice.toFixed(2).toString().replace('.', ',')+'€')
 
     // updateTotalPrice();
-};
+
+    localStorage.setItem('cart-products', JSON.stringify(cartProductsToBuy));
+
+    updateCartCounter();
+}
 
 function removeToCartElement(id) {
     cartProductsToBuy = cartProductsToBuy.filter((i) => i.id !== id);
@@ -308,7 +366,6 @@ function cartItemActionQuantity(type, id) {
     const isInCart = cartProductsToBuy.find((i) => i.id === id);
 
     if (type === 'plus') {
-
         isInCart.count += 1;
     } else {
         if (isInCart.count === 1 ) { return }
@@ -319,12 +376,70 @@ function cartItemActionQuantity(type, id) {
     addToCart()
 }
 
-
-function addToCartExample(id) {
+function addToCartProcessor(id) {
     const productToAddToCart = products.find((product) => product.id === id);
     addToCart(productToAddToCart)
-};
+    addToCartAnimation(id)
+}
 
+function addToCartAnimation(id) {
+        let cart = $('#cart-button-action');
+        let imgtodrag = $('.card-product[data-id="'+id+'"]').find("img").eq(0);
+        if (imgtodrag) {
+            let imgclone = imgtodrag.clone()
+                .offset({
+                    top: imgtodrag.offset().top,
+                    left: imgtodrag.offset().left
+                })
+                .css({
+                    'opacity': '0.7',
+                    'position': 'absolute',
+                    'height': '460px',
+                    'width': '340px',
+                    'z-index': '100'
+                })
+                .appendTo($('body'))
+                .animate({
+                    'top': cart.offset().top + 31,
+                    'left': cart.offset().left + 15,
+                    'width': 80,
+                    'height': 90
+                }, 1000);
 
-// go to top button
+            setTimeout(function () {
+                cart.effect("bounce", {
+                    times: 1
+                });
+            }, 1500);
+
+            imgclone.animate({
+                'width': 0,
+                'height': 0
+            }, function () {
+                $(this).detach()
+            });
+        }
+}
+
+function updateCartCounter() {
+    let cartCounter = $('#cart-counter');
+    let totalItems = 0;
+
+    cartProductsToBuy.forEach((product) => {
+        totalItems += product.count;
+    })
+
+    if (totalItems) {
+        cartCounter.text(totalItems > 9 ? " " : totalItems);
+        cartCounter.show();
+    }
+    else {
+        cartCounter.text(0);
+        cartCounter.hide();
+    }
+}
+
+function continueAlert() {
+    alert("It works! You would be redirected to Payment Page if it existed.");
+}
 
